158ef706a64ec4ae8ec2b04eae16780d4cf20f3e
sgiarola <samuel.giarola@gmail.com>
2018-01-14 11:43:46 -0200

    Fix rest /user method to use path param.

--- webchat-controller/src/main/java/com/challenge/webchat/controller/user/UserController.java
+++ webchat-controller/src/main/java/com/challenge/webchat/controller/user/UserController.java
@@ -1,15 +1,12 @@
 package com.challenge.webchat.controller.user;
 
 import com.challenge.webchat.business.user.UserBusiness;
-import com.challenge.webchat.commons.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -18,10 +15,8 @@ public class UserController {
     @Autowired
     private UserBusiness userBusiness;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity getUser(@RequestBody Map<String, String> request) {
-        String username = request.get("username");
-        User user = userBusiness.getUserBy(username);
-        return ResponseEntity.ok().body(user);
+    @RequestMapping(method = RequestMethod.GET, value = "/{username}")
+    public ResponseEntity getUser(@PathVariable("username") String username) {
+        return ResponseEntity.ok().body(userBusiness.getUserBy(username));
     }
 }
